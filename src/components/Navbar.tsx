ae07bbec18584fb71bd1a00ec3f38b8f05f52329
Pramuditha Karunarathna <shemil@Pramudithas-MacBook-Pro.local>
2024-08-23 16:11:37 +0530

    Nav bar changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,16 +32,28 @@ const Navbar: React.FC = () => {
   };
 
   const openShoeModal = () => setIsShoeModal(true);
-  const closeShoeModal = () => setIsShoeModal(false);
+  const closeShoeModal = () => {
+    setIsShoeModal(false)
+    closeMenu();
+  };
 
   const openCreateOrderModal = () => setIsCreateOrderModal(true);
-  const closeCreateOrderModal = () => setIsCreateOrderModal(false);
+  const closeCreateOrderModal = () => {
+    setIsCreateOrderModal(false);
+    closeMenu();
+  }
 
   const openUpdateOrderModal = () => setIsUpdateOrderModal(true);
-  const closeUpdateOrderModal = () => setIsUpdateOrderModal(false);
+  const closeUpdateOrderModal = () => {
+    setIsUpdateOrderModal(false);
+    closeMenu();
+  }
 
   const openSignInModal = () => setIsSignInModal(true);
-  const closeSignInModal = () => setIsSignInModal(false);
+  const closeSignInModal = () => {
+    setIsSignInModal(false);
+    closeMenu();
+  };
 
   const handleLogout = () => {
     dispatch(adminLogout());
@@ -50,41 +62,61 @@ const Navbar: React.FC = () => {
 
   return (
     <nav className="navbar">
-      <div className="navbar-header">
+      <div className={`navbar-header ${isMenuOpen ? 'active' : ''}`}>
         <Link to="/">
           <img src="/assets/black-logo.png" alt="logo" className="logo" />
         </Link>
-        <div className="burger-menu" onClick={toggleMenu}>
-          <div className={`bar ${isMenuOpen ? 'change' : ''}`}></div>
-          <div className={`bar ${isMenuOpen ? 'change' : ''}`}></div>
-          <div className={`bar ${isMenuOpen ? 'change' : ''}`}></div>
-        </div>
+        <div className={`nav__hamburger ${isMenuOpen ? "active" : ""}`} onClick={toggleMenu}>
+        <span className="nav__hamburger__line"></span>
+        <span className="nav__hamburger__line"></span>
+        <span className="nav__hamburger__line"></span>
+      </div>
       </div>
       <div className={`navbar-links ${isMenuOpen ? 'active' : ''}`}>
+        <ul>
+        <li>
         <Link to="/" className="navbar-link" onClick={closeMenu}>Home</Link>
+        </li>
+        <li>
         <Link to="/product-page" className="navbar-link" onClick={closeMenu}>Products</Link>
+        </li>
+        <li>
         <Link to="/track-order" className="navbar-link" onClick={closeMenu}>Track Order</Link>
+        </li>
+        <li onClick={closeMenu}>
         <a href="https://drive.google.com/file/d/1Jfc096_gil8biMO2kJmXsb5LPz8cllI7/view" target="_blank" rel="noopener noreferrer" className="navbar-link">Return Policy</a>
+        </li>
 
         {!adminToken ? (
           <>
+            <li>
             <Button className="regular-black-button" onClick={openSignInModal}>Sign In</Button>
+            </li>
             <SignInModal isOpen={isSignInModal} onClose={closeSignInModal} />
           </>
         ) : (
-          <Button className="regular-black-button" onClick={handleLogout}>Sign Out</Button>
+          <li>
+            <Button className="regular-black-button" onClick={handleLogout}>Sign Out</Button>
+          </li>
         )}
 
         {adminToken && (
           <div className="admin-button-container">
+            <li>
             <Button className="regular-black-button" onClick={openShoeModal}>Add Product</Button>
+            </li>
+            <li>
             <Button className="regular-black-button" onClick={openCreateOrderModal}>Create Order</Button>
+            </li>
+            <li>
             <Button className="regular-black-button" onClick={openUpdateOrderModal}>Update Order</Button>
+            </li>
             <AddNewProductModal isOpen={isShoeModal} onClose={closeShoeModal} />
             <CreateOrderModal shoes={shoes} isOpen={isCreateOrderModal} onClose={closeCreateOrderModal} />
             <UpdateOrderStatusModal isOpen={isUpdateOrderModal} onClose={closeUpdateOrderModal} />
           </div>
         )}
+        </ul>
       </div>
     </nav>
   );
